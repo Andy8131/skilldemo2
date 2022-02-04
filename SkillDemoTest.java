c6dd3e78cccfe46b28f6c20383ae3f4ac28f4e93
Andy8131 <andy.liu0123456@gmail.com>
2022-02-03 22:29:41 -0800

    added lib + test files + junit

---
+++ SkillDemoTest.java
@@ -0,0 +1,10 @@
+import static org.junit.Assert.*;
+ 
+import org.junit.*;
+
+public class SkillDemoTest {
+   @Test 
+   public void Test1(){
+       assertEquals(1, SkillDemo.subtract(10, 3));
+   } 
+}
